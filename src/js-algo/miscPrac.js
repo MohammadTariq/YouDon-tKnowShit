fe34b2339c4bec0ee2a4449a8c6590e927fed18c
manavisingh <ms1576@gmail.com>
2018-09-10 19:13:34 +0530

    Adding LL files

--- src/js-algo/miscPrac.js
+++ src/js-algo/miscPrac.js
@@ -1,23 +1,8 @@
-function is(arr){
-    for (var i = 1; i < arr.length; i++){
-        var key = arr[i];
-        var j = i - 1;
-        while(j >= 0 && arr[j] > key){
-            arr[j + 1] = arr[j];
-            j = j - 1;
-        }
-        arr[j+1] = key;
-    }
-    return arr;
-}
-
-is([1,2,5,3,8,8,5,2,9,5]);
-
 function ss(arr){
     for (var i = 0; i < arr.length; i++){
         var min = i;
-        for (var j = i + 1; j < arr.length; j++){
-            if (arr[j] < arr[min]){
+        for (var j = i+1; j < arr.length; j++){
+            if (arr[j]<arr[min]){
                 min = j;
             }
         }
@@ -30,4 +15,19 @@ function ss(arr){
     return arr;
 }
 
-ss([1,2,5,3,8,8,5,2,9,5]);
+ss([1,2,4,5,2,7,5,4,1,9,8,8]);
+
+function is(arr){
+    for (var i = 0; i < arr.length; i++){
+        var key = arr[i];
+        var j = i - 1;
+        while(j >= 0 && arr[j] > key){
+            arr[j+1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+    return arr;
+}
+
+is([1,2,4,5,2,7,5,4,1,9,8,8]);
